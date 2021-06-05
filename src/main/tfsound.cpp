ccc7b0358c410de1e116db8bd0967bb39ef41152
jonathanmarp <jonathanmarpaung88@gmail.com>
2021-06-05 18:23:46 +0700

    I just wont to commit this. I will be continue at the moments I like

--- src/main/tfsound.cpp
+++ src/main/tfsound.cpp
@@ -1,1 +1,76 @@
+// MIT License
+
+// Copyright (c) 2021 laferenorg
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include <iostream>
+#include <string>
+
+int main(int argc, const char* argv[]) {
+	// init
+	const char* argv_copy[argc - 1];
+	for(int i = 1; i < argc; i++) argv_copy[i - 2] = argv[i];
+	int length_argv_copy = argc - 2;
+
+	// check argc
+	switch(argc) {
+		case 1: {
+			printf("Giving Parmeter (--play, --compile) <filepath>\n");
+			/* End Of Program And Clean */
+			{
+				(void)argc;
+				(void)argv;
+			}
+			return 0;
+		}
+		break;
+		case 2: {
+			printf("Giving Parmeter <filepath>\n");
+			/* End Of Program And Clean */
+			{
+				(void)argc;
+				(void)argv;
+			}
+			return 0;
+		}
+		break;
+	}
+
+	// Check
+	std::string argv_cheking = argv[1];
+	if(argv_cheking == "--play")
+	{
+
+	}
+	else if(argv_cheking == "--compile")
+	{
+
+	}
+	else
+	{
+		std::cout << "Undefined Parameter 1 : " << argv[1] << std::endl;
+	}
+
+	{
+		(void)argc;
+		(void)argv;
+	}
+	return 0;
+}
